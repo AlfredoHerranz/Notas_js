529d29522c467e781bb6451613a0a0f098d2ea2a
Iván <ivan.ruicos@educa.jcyl.es>
2024-02-01 17:10:20 +0100

    Nuevo

---
+++ php/select.php
@@ -0,0 +1,21 @@
+<?php 
+    include_once("db.php");
+
+    class modificar_tarea{
+        private $db_handler;
+
+        public function __construct(){
+            $this -> db_handler = db::conectar();
+        }
+
+        public function mostrarDatos(){
+            $pdo = $this -> db_handler -> prepare("SELECT * FROM tareas");
+            $pdo -> execute();
+            $devolver = $pdo->fetchAll(PDO::FETCH_ASSOC);
+            return $devolver;
+        }
+    }
+
+    $tarea = new modificar_tarea();
+    print_r($tarea->mostrarDatos());
+?>
